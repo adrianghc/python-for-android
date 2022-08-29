506e08ad6127aaeac3f2b4865703a965123df160
Adrian Herrmann <adrian.herrmann@qt.io>
2022-08-29 18:30:09 +0200

    Update wheel paths for shiboken/PySide6

--- pythonforandroid/recipes/shiboken6/__init__.py
+++ pythonforandroid/recipes/shiboken6/__init__.py
@@ -1,10 +1,12 @@
 from pythonforandroid.recipe import PythonRecipe
 from pythonforandroid.logger import info
 import zipfile
+from pythonforandroid.toolchain import shprint
+import sh
 
 class ShibokenRecipe(PythonRecipe):
     version = '6.3.1'
-    wheel_path = '/home/shyamnath/qt_for_python/pyside-setup/dist/shiboken6-6.3.1-6.3.1-cp36-abi3-android_x86_64.whl'
+    wheel_path = '/home/adrianghc/wheels/shiboken6-6.3.1-6.3.1-cp36-abi3-android_x86_64.whl'
 
     call_hostpython_via_targetpython = False
     install_in_hostpython = False
